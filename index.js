544ac57245572d470b78ccdddb009ba64c12faf6
kalunge <muthomititus+github@gmail.com>
2022-06-08 14:37:09 +0300

    Done with functions lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+let drivers = ["Titus", "Muthomi", "Kalunge", "Talib", "Faith"];
+const returnFirstTwoDrivers = (drivers) => {
+  return [drivers[0], drivers[1]];
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+  return function () {
+    return multiplier ** 2;
+  };
+};
+
+const fareDoubler = (fare) => {
+  return fare * 2;
+};
+
+const fareTripler = (fare) => fare * 3;
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+  return func(arrayOfDrivers);
+}
